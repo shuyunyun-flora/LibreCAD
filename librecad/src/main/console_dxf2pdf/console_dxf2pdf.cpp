5eed7c362a6aa4eef439769111c53de56fbf5504
shuyunyun-flora <shuyunyun_flora@live.com>
2024-05-17 20:28:51 +0800

    CQ00000000: misc.

--- librecad/src/main/console_dxf2pdf/console_dxf2pdf.cpp
+++ librecad/src/main/console_dxf2pdf/console_dxf2pdf.cpp
@@ -46,144 +46,144 @@ static void parseMarginsArg(QString, PdfPrintParams&);
 
 int console_dxf2pdf(int argc, char* argv[])
 {
-    RS_DEBUG->setLevel(RS_Debug::D_NOTHING);
+    //RS_DEBUG->setLevel(RS_Debug::D_NOTHING);
 
     QApplication app(argc, argv);
-    QCoreApplication::setOrganizationName("LibreCAD");
-    QCoreApplication::setApplicationName("LibreCAD");
-    QCoreApplication::setApplicationVersion(XSTR(LC_VERSION));
+    //QCoreApplication::setOrganizationName("LibreCAD");
+    //QCoreApplication::setApplicationName("LibreCAD");
+    //QCoreApplication::setApplicationVersion(XSTR(LC_VERSION));
 
-    QFileInfo prgInfo(QFile::decodeName(argv[0]));
-    RS_SETTINGS->init(app.organizationName(), app.applicationName());
-    RS_SYSTEM->init( app.applicationName(), app.applicationVersion(), XSTR(QC_APPDIR), argv[0]);
+    //QFileInfo prgInfo(QFile::decodeName(argv[0]));
+    //RS_SETTINGS->init(app.organizationName(), app.applicationName());
+    //RS_SYSTEM->init( app.applicationName(), app.applicationVersion(), XSTR(QC_APPDIR), argv[0]);
 
-    QCommandLineParser parser;
+    //QCommandLineParser parser;
 
-    QStringList appDesc;
-    QString librecad( prgInfo.filePath());
-    if (prgInfo.baseName() != "dxf2pdf") {
-        librecad += " dxf2pdf"; // executable is not dxf2pdf, thus argv[1] must be 'dxf2pdf'
-        appDesc << "";
-        appDesc << "dxf2pdf " + QObject::tr( "usage: ") + librecad + QObject::tr( " [options] <dxf_files>");
-    }
-    appDesc << "";
-    appDesc << "Print a bunch of DXF files to PDF file(s).";
-    appDesc << "";
-    appDesc << "Examples:";
-    appDesc << "";
-    appDesc << "  " + librecad + QObject::tr( " *.dxf");
-    appDesc << "    " + QObject::tr( "-- print all dxf files to pdf files with the same names.");
-    appDesc << "";
-    appDesc << "  " + librecad + QObject::tr( " -o some.pdf *.dxf");
-    appDesc << "    " + QObject::tr( "-- print all dxf files to 'some.pdf' file.");
-    parser.setApplicationDescription( appDesc.join( "\n"));
+    //QStringList appDesc;
+    //QString librecad( prgInfo.filePath());
+    //if (prgInfo.baseName() != "dxf2pdf") {
+    //    librecad += " dxf2pdf"; // executable is not dxf2pdf, thus argv[1] must be 'dxf2pdf'
+    //    appDesc << "";
+    //    appDesc << "dxf2pdf " + QObject::tr( "usage: ") + librecad + QObject::tr( " [options] <dxf_files>");
+    //}
+    //appDesc << "";
+    //appDesc << "Print a bunch of DXF files to PDF file(s).";
+    //appDesc << "";
+    //appDesc << "Examples:";
+    //appDesc << "";
+    //appDesc << "  " + librecad + QObject::tr( " *.dxf");
+    //appDesc << "    " + QObject::tr( "-- print all dxf files to pdf files with the same names.");
+    //appDesc << "";
+    //appDesc << "  " + librecad + QObject::tr( " -o some.pdf *.dxf");
+    //appDesc << "    " + QObject::tr( "-- print all dxf files to 'some.pdf' file.");
+    //parser.setApplicationDescription( appDesc.join( "\n"));
 
-    parser.addHelpOption();
-    parser.addVersionOption();
+    //parser.addHelpOption();
+    //parser.addVersionOption();
 
-    QCommandLineOption fitOpt(QStringList() << "a" << "fit",
-        QObject::tr( "Auto fit and center drawing to page."));
-    parser.addOption(fitOpt);
+    //QCommandLineOption fitOpt(QStringList() << "a" << "fit",
+    //    QObject::tr( "Auto fit and center drawing to page."));
+    //parser.addOption(fitOpt);
 
-    QCommandLineOption centerOpt(QStringList() << "c" << "center",
-        QObject::tr( "Auto center drawing on page."));
-    parser.addOption(centerOpt);
+    //QCommandLineOption centerOpt(QStringList() << "c" << "center",
+    //    QObject::tr( "Auto center drawing on page."));
+    //parser.addOption(centerOpt);
 
-    QCommandLineOption grayOpt(QStringList() << "k" << "grayscale",
-        QObject::tr( "Print grayscale."));
-    parser.addOption(grayOpt);
+    //QCommandLineOption grayOpt(QStringList() << "k" << "grayscale",
+    //    QObject::tr( "Print grayscale."));
+    //parser.addOption(grayOpt);
 
-    QCommandLineOption monoOpt(QStringList() << "m" << "monochrome",
-        QObject::tr( "Print monochrome (black/white)."));
-    parser.addOption(monoOpt);
+    //QCommandLineOption monoOpt(QStringList() << "m" << "monochrome",
+    //    QObject::tr( "Print monochrome (black/white)."));
+    //parser.addOption(monoOpt);
 
-    QCommandLineOption pageSizeOpt(QStringList() << "p" << "paper",
-        QObject::tr( "Paper size (Width x Height) in mm.", "WxH"));
-    parser.addOption(pageSizeOpt);
+    //QCommandLineOption pageSizeOpt(QStringList() << "p" << "paper",
+    //    QObject::tr( "Paper size (Width x Height) in mm.", "WxH"));
+    //parser.addOption(pageSizeOpt);
 
-    QCommandLineOption resOpt(QStringList() << "r" << "resolution",
-        QObject::tr( "Output resolution (DPI).", "integer"));
-    parser.addOption(resOpt);
+    //QCommandLineOption resOpt(QStringList() << "r" << "resolution",
+    //    QObject::tr( "Output resolution (DPI).", "integer"));
+    //parser.addOption(resOpt);
 
-    QCommandLineOption scaleOpt(QStringList() << "s" << "scale",
-        QObject::tr( "Output scale. E.g.: 0.01 (for 1:100 scale)."), "double");
-    parser.addOption(scaleOpt);
+    //QCommandLineOption scaleOpt(QStringList() << "s" << "scale",
+    //    QObject::tr( "Output scale. E.g.: 0.01 (for 1:100 scale)."), "double");
+    //parser.addOption(scaleOpt);
 
-    QCommandLineOption marginsOpt(QStringList() << "f" << "margins",
-        QObject::tr( "Paper margins in mm (integer or float)."), "L,T,R,B");
-    parser.addOption(marginsOpt);
+    //QCommandLineOption marginsOpt(QStringList() << "f" << "margins",
+    //    QObject::tr( "Paper margins in mm (integer or float)."), "L,T,R,B");
+    //parser.addOption(marginsOpt);
 
-    QCommandLineOption pagesNumOpt(QStringList() << "z" << "pages",
-        QObject::tr( "Print on multiple pages (Horiz. x Vert.)."), "HxV");
-    parser.addOption(pagesNumOpt);
+    //QCommandLineOption pagesNumOpt(QStringList() << "z" << "pages",
+    //    QObject::tr( "Print on multiple pages (Horiz. x Vert.)."), "HxV");
+    //parser.addOption(pagesNumOpt);
 
-    QCommandLineOption outFileOpt(QStringList() << "o" << "outfile",
-        QObject::tr( "Output PDF file.", "file"));
-    parser.addOption(outFileOpt);
+    //QCommandLineOption outFileOpt(QStringList() << "o" << "outfile",
+    //    QObject::tr( "Output PDF file.", "file"));
+    //parser.addOption(outFileOpt);
 
-    QCommandLineOption outDirOpt(QStringList() << "t" << "directory",
-        QObject::tr( "Target output directory."), "path");
-    parser.addOption(outDirOpt);
+    //QCommandLineOption outDirOpt(QStringList() << "t" << "directory",
+    //    QObject::tr( "Target output directory."), "path");
+    //parser.addOption(outDirOpt);
 
-    parser.addPositionalArgument(QObject::tr( "<dxf_files>"), QObject::tr( "Input DXF file(s)"));
+    //parser.addPositionalArgument(QObject::tr( "<dxf_files>"), QObject::tr( "Input DXF file(s)"));
 
-    parser.process(app);
+    //parser.process(app);
 
-    const QStringList args = parser.positionalArguments();
+    //const QStringList args = parser.positionalArguments();
 
-    if (args.isEmpty() || (args.size() == 1 && args[0] == "dxf2pdf"))
-        parser.showHelp(EXIT_FAILURE);
+    //if (args.isEmpty() || (args.size() == 1 && args[0] == "dxf2pdf"))
+    //    parser.showHelp(EXIT_FAILURE);
 
-    PdfPrintParams params;
+    //PdfPrintParams params;
 
-    params.fitToPage = parser.isSet(fitOpt);
-    params.centerOnPage = parser.isSet(centerOpt);
-    params.grayscale = parser.isSet(grayOpt);
-    params.monochrome = parser.isSet(monoOpt);
-    params.pageSize = parsePageSizeArg(parser.value(pageSizeOpt));
+    //params.fitToPage = parser.isSet(fitOpt);
+    //params.centerOnPage = parser.isSet(centerOpt);
+    //params.grayscale = parser.isSet(grayOpt);
+    //params.monochrome = parser.isSet(monoOpt);
+    //params.pageSize = parsePageSizeArg(parser.value(pageSizeOpt));
 
-    bool resOk;
-    int res = parser.value(resOpt).toInt(&resOk);
-    if (resOk)
-        params.resolution = res;
+    //bool resOk;
+    //int res = parser.value(resOpt).toInt(&resOk);
+    //if (resOk)
+    //    params.resolution = res;
 
-    bool scaleOk;
-    double scale = parser.value(scaleOpt).toDouble(&scaleOk);
-    if (scaleOk)
-        params.scale = scale;
+    //bool scaleOk;
+    //double scale = parser.value(scaleOpt).toDouble(&scaleOk);
+    //if (scaleOk)
+    //    params.scale = scale;
 
-    parseMarginsArg(parser.value(marginsOpt), params);
-    parsePagesNumArg(parser.value(pagesNumOpt), params);
+    //parseMarginsArg(parser.value(marginsOpt), params);
+    //parsePagesNumArg(parser.value(pagesNumOpt), params);
 
-    params.outFile = parser.value(outFileOpt);
-    params.outDir = parser.value(outDirOpt);
+    //params.outFile = parser.value(outFileOpt);
+    //params.outDir = parser.value(outDirOpt);
 
-    for (auto arg : args) {
-        QFileInfo dxfFileInfo(arg);
-        if (dxfFileInfo.suffix().toLower() != "dxf")
-            continue; // Skip files without .dxf extension
-        params.dxfFiles.append(arg);
-    }
+    //for (auto arg : args) {
+    //    QFileInfo dxfFileInfo(arg);
+    //    if (dxfFileInfo.suffix().toLower() != "dxf")
+    //        continue; // Skip files without .dxf extension
+    //    params.dxfFiles.append(arg);
+    //}
 
-    if (params.dxfFiles.isEmpty())
-        parser.showHelp(EXIT_FAILURE);
+    //if (params.dxfFiles.isEmpty())
+    //    parser.showHelp(EXIT_FAILURE);
 
-    if (!params.outDir.isEmpty()) {
-        // Create output directory
-        if (!QDir().mkpath(params.outDir)) {
-            qDebug() << "ERROR: Cannot create directory" << params.outDir;
-            return EXIT_FAILURE;
-        }
-    }
+    //if (!params.outDir.isEmpty()) {
+    //    // Create output directory
+    //    if (!QDir().mkpath(params.outDir)) {
+    //        qDebug() << "ERROR: Cannot create directory" << params.outDir;
+    //        return EXIT_FAILURE;
+    //    }
+    //}
 
-    RS_FONTLIST->init();
-    RS_PATTERNLIST->init();
+    //RS_FONTLIST->init();
+    //RS_PATTERNLIST->init();
 
-    PdfPrintLoop *loop = new PdfPrintLoop(params, &app);
+    //PdfPrintLoop *loop = new PdfPrintLoop(params, &app);
 
-    QObject::connect(loop, SIGNAL(finished()), &app, SLOT(quit()));
+    //QObject::connect(loop, SIGNAL(finished()), &app, SLOT(quit()));
 
-    QTimer::singleShot(0, loop, SLOT(run()));
+    //QTimer::singleShot(0, loop, SLOT(run()));
 
     return app.exec();
 }
